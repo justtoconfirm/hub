b155b8b6b81f6f23046a742976b2d95e1a5db80d
Justtoconfirm <office@justtoconfirm.co.uk>
2017-12-05 20:17:37 +0000

    Signup template component added and routing applied

---
+++ hub-project/src/components/Signup.vue
@@ -0,0 +1,24 @@
+<template>
+	<div class="login">
+		<h3>Sign Up</h3>
+		<p>Let's create a new account</p>
+		<input type="text" placeholder="Email"><br/>
+		<input type="password" placeholder="Password"><br/>
+		<button>Sign Up</button>
+		<p>or go back to login.</p>
+	</div>
+</template>
+
+<script>
+  export default {
+    name: 'signup',
+    data: function () {
+      return {}
+    },
+    methods: {}
+  }
+</script>
+
+<style scoped>
+/* "scoped" attribute limits the CSS to this component only */
+</style>
